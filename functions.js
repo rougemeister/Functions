6ec5b40f09b5c4e7374fe330dec08152bfceaef3
kruzznitsch <rougemeister1@gmail.com>
2024-06-11 21:19:35 +0100

    Added reverseString function

--- functions.js
+++ functions.js
@@ -5,6 +5,18 @@
 
 // A function to Capitalizes the first letter of a string.
 function capitalize (str ) {
+    if(typeof str !== 'string') return "Input not a string"
     return str[0].toUpperCase() + str.slice(1);
+}
+
+
+// A function to reverse a given string
+function reverseString (str){
+
+    let newStr = ''
+    for(let i = str.length - 1; i >= 0; i--){
+        newStr += str[i]
+    }
+    return newStr
 }
 
