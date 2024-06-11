ff63cbd9f40d30890a38d1afc97722e56514901f
kruzznitsch <rougemeister1@gmail.com>
2024-06-11 21:23:12 +0100

    Updated reverse function

--- functions.js
+++ functions.js
@@ -11,7 +11,12 @@ function capitalize (str ) {
 
 
 // A function to reverse a given string
-function reverseString (str){
+function reverse(str){
+    if(str === '') {
+        return 'Empty string'
+    }else if (typeof str !== 'string'){
+        return 'Input not a string!'
+    }
 
     let newStr = ''
     for(let i = str.length - 1; i >= 0; i--){
@@ -19,4 +24,6 @@ function reverseString (str){
     }
     return newStr
 }
+
+console.log(reverse(8))
 
